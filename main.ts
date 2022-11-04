7d1e0755b6a904a6a5cc268d2c6bdde242659cc3
tuttakatanonnon <117436570+tuttakatanonnon@users.noreply.github.com>
2022-11-04 17:21:47 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,110 @@
+basic.forever(function () {
+    basic.showString("boku")
+    basic.showString("meishi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("tokorode")
+    basic.showString("hukushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("totemo")
+    basic.showString("hukushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("neko")
+    basic.showString("keiyoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("rareru")
+    basic.showString("jyodoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("kanasii")
+    basic.showString("keiyoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("dono")
+    basic.showString("jyoshi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("asobu")
+    basic.showString("doushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("mosshimoshi")
+    basic.showString("kandoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("mosshimoshi")
+    basic.showString("kandoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("mosshimoshi")
+    basic.showString("kandoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+    basic.showString("kireida")
+    basic.showString("keiyoudoushi?")
+    basic.pause(5000)
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    basic.pause(2000)
+})
